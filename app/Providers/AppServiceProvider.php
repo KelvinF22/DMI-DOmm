8d6f46fb41494e08df1049ecbd97ef9ab716f8d2
Kelvinx20a <kelvinx20a@gmail.com>
2026-04-20 15:16:17 +0700

    nambah url php di app service

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; // <--- PASTIKAN BARIS INI ADA!
 
 class AppServiceProvider extends ServiceProvider
 {
